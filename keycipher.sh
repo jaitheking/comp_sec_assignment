9042ff2f4bb23447b6d9529c117ead50229ece98
Jaiprashanthgit config --global user.name Jaiprashanthgit config --global user.name Jaiprashanth <jaitheking4858@gmail.com>
2014-12-14 15:17:20 +0800

    Added Decryption feature to keycipher.sh and created interface for both cipher methods

--- keycipher.sh
+++ keycipher.sh
@@ -1,25 +1,66 @@
 #!/bin/bash
-key=`echo $1 | tr '[:lower:]' '[:upper:]' `
-P="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-mkey=$key$P
-#Make the cipher
-for ((i=0;i<${#mkey};++i)); do
-    c=${mkey:i:1}
-    tailmkey=${mkey:i+1}
-    mkey=${mkey::i+1}${tailmkey//"$c"/}
+#Key Cipher Menu
+echo "Key Cipher Encryption & Decryption Tool"
+echo "1.Encrypt Text File."
+echo "2.Decrypt Text File."
+#Read the type of operation the user wants to do
+read keyop;
+#Selection Menu Implementation
+case $keyop  in 
 
-done
+		1) 	echo "Enter a key word"
+			read  keyword;
+					
+		  	echo "Enter the location of the text file to be encrypted (eg: ~/sample.txt) "	
+	               read dir;
+
+			key=`echo $keyword | tr '[:lower:]' '[:upper:]' `
+			P="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			mkey=$key$P
+			#Make the cipher
+			for ((i=0;i<${#mkey};++i)); do
+		    	c=${mkey:i:1}
+		    	tailmkey=${mkey:i+1}
+		    	mkey=${mkey::i+1}${tailmkey//"$c"/}
+
+			done
+
+			#Do Substitution
+			echo $mkey | tr '[:lower:]' '[:upper:]'
+
+			OUT=`cat $dir | tr '[:lower:]' '[:upper:]' | tr $P $mkey`
+			echo $OUT >>encrypted.txt		
+			echo "File has been encrypted and saved as 'encrypted.txt' ."
+			;;
+		2)
+			echo "Enter the key word"
+			read  keyword;
+				
+		  	echo "Enter the location of the text file (eg: ~/encrypted.txt) "	
+	               read dir;
+
+			key=`echo $keyword | tr '[:lower:]' '[:upper:]' `
+			P="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			mkey=$key$P
+			#Make the cipher
+			for ((i=0;i<${#mkey};++i)); do
+		    	c=${mkey:i:1}
+		    	tailmkey=${mkey:i+1}
+		    	mkey=${mkey::i+1}${tailmkey//"$c"/}
+
+			done
+
+			#Do Substitution
+			echo $mkey | tr '[:lower:]' '[:upper:]'
+
+			OUT=`cat $dir | tr '[:lower:]' '[:upper:]' | tr $mkey $P`
+			echo $OUT >>decrypted.txt		
+			echo "File has been decrypted and saved as 'decrypted.txt' ."
+			;;
+
+esac
 
 
 
-
-#Do Substitution
-echo $mkey | tr '[:lower:]' '[:upper:]'
-
-OUT=`cat $2 | tr '[:lower:]' '[:upper:]' | tr $P $mkey`
-echo $OUT >>encrypted.txt
-
-#Reversal
-#echo $OUT | tr $C $P
 
 
--- railfence.sh
+++ railfence.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 #Prompts user for location
-echo "Rail Fence Cipher Encryption"
+echo "Rail Fence Cipher Encryption & Decryption Tool"
 echo "Enter the plaintext location: (eg:~/file.txt)"
-#reads input
-read string
-stringZ=`cat $string | tr -d ' ' `
-#encrypting
-row1=${stringZ:0:1}${stringZ:6:1}${stringZ:12:1}${stringZ:18:1}
-row2=${stringZ:1:1}${stringZ:5:1}${stringZ:7:1}${stringZ:11:1}${stringZ:13:1}${stringZ:17:1}${stringZ:19:1}
-row3=${stringZ:2:1}${stringZ:4:1}${stringZ:8:1}${stringZ:10:1}${stringZ:14:1}${stringZ:16:1}${stringZ:20:1}
-row4=${stringZ:3:1}${stringZ:9:1}${stringZ:15:1}${stringZ:21:1}
+echo "1.Encrypt Text File."
+echo "2.Decrypt Text File."
+#Read the type of operation the user wants to do
+read fenceop;
 
-cipher=$row1$row2$row3$row4
+#Selection Menu Implementation
+case $fenceop  in 
+			1)	echo "Please enter the location of the textfile you wish to encrypt (eg. ~/sample.txt)"
+				#reads input
+				read string
+				stringZ=`cat $string | tr -d ' ' | tr '[:lower:]' '[:upper:]'`
+				#Encrypting via transposition of original plaintext
+				row1=${stringZ:0:1}${stringZ:6:1}${stringZ:12:1}${stringZ:18:1}
+				row2=${stringZ:1:1}${stringZ:5:1}${stringZ:7:1}${stringZ:11:1}${stringZ:13:1}${stringZ:17:1}${stringZ:19:1}
+				row3=${stringZ:2:1}${stringZ:4:1}${stringZ:8:1}${stringZ:10:1}${stringZ:14:1}${stringZ:16:1}${stringZ:20:1}
+				row4=${stringZ:3:1}${stringZ:9:1}${stringZ:15:1}${stringZ:21:1}
 
-echo "$cipher" >> encrypted.txt
+				#Concatenating the string after permutation
+				cipher=$row1$row2$row3$row4
+
+				echo "$cipher" >> encrypted.txt
+				echo "Encryption is successful. The text file is saved as encrypted.txt"
+				;;
+
+
+			2) echo "Not Ready Yet"
+			;;
+
+esac
+
+
+
